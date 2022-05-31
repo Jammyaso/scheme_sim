51e2faadf765ed9a6e1c9a6c0afcbbd8728661d4
Jammyaso <Jam_myaso@mail.ru>
2022-05-31 16:36:46 +0500

    add more selects and cleaning

--- test.js
+++ test.js
@@ -3,11 +3,12 @@ var Pins = {
     "path": [],
     "wire": "",
     "isVisible" : false,
-    render: function(ctx){
+    render: function(ctx, ukey = null){
         if (!this.isVisible) return;
         
         //ctx = document.getElementById("canvas").getContext("2d");
-        ctx.clearRect(0,0,canvas.width,canvas.height); 
+        //ctx.clearRect(0,0,canvas.width,canvas.height); 
+        //console.log("rendered - " + ukey)
         ctx.beginPath();
         ctx.moveTo(this.path[0].x, this.path[0].y);
         this.path.forEach(function(element, key){
@@ -31,29 +32,82 @@ window.onload = function(){
     var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext('2d');
 
+    var optionElement = document.getElementById("select");
+    var wiresElement = document.getElementById("wires");
+    var pointsElement = document.getElementById("points");
+    var wiresList = [];
+    var pointsList = [];
+
     Object.keys(connections).forEach(function(key) {
         Object.setPrototypeOf(connections[key], Pins);
-        option = document.createElement("option");
+
+        option = document.createElement("option");    
         option.text = "["+connections[key].wire+"] "+key;
         option.setAttribute("value", key);
-        document.getElementById("select").appendChild(option);
+        optionElement.appendChild(option);
+        
+        
+        if (!wiresList[connections[key].wire]) {
+            wiresList[connections[key].wire] = 1;
+        } else {
+            wiresList[connections[key].wire]++;
+        }
+
+        if (!pointsList[connections[key].points[0].point]) {
+            pointsList[connections[key].points[0].point] = 1;
+        } else {
+            pointsList[connections[key].points[0].point]++;
+        }
+        if (!pointsList[connections[key].points[1].point]) {
+
+            pointsList[connections[key].points[1].point] = 1;
+        } else {
+            pointsList[connections[key].points[1].point]++;
+        }
+        
     })
+    Object.keys(wiresList).forEach(function(key) {
+        option = document.createElement("option");   
+        option.text = key+" - "+wiresList[key];
+        option.setAttribute("value", key);
+        wiresElement.appendChild(option);
+    })
+    Object.keys(pointsList).forEach(function(key) {
+        option = document.createElement("option");   
+        option.text = key+" - "+pointsList[key];
+        option.setAttribute("value", key);
+        pointsElement.appendChild(option);
+    })
+    
+
     renderAll = function(){
         ctx.clearRect(0,0,canvas.width,canvas.height);
         Object.keys(connections).forEach(function(key) {
-            if (connections[key]) connections[key].render(ctx);
+            if (connections[key]) connections[key].render(ctx, key);
         })
     
     }
     renderAll();
+
     document.getElementById("canvas").onclick = function(e){
         console.log('{"x":'+e.pageX+',  "y":'+e.pageY+'},');
     }
-    document.getElementById("select").onchange = function(e){
+    optionElement.onchange = function(e){
         Object.keys(connections).forEach(function(key) {
             connections[key].isVisible = false;
         })
-        connections[document.getElementById("select").value].isVisible = true;
+        connections[optionElement.value].isVisible = true;
         renderAll();
     }
+    wiresElement.onchange = function(e){
+        Object.keys(connections).forEach(function(key) {
+            connections[key].isVisible = false;
+            if ( connections[key].wire == wiresElement.value){
+                connections[key].isVisible = true;
+            }
+        })
+        console.log(connections);
+        renderAll();
+    }
+    
 }
